bb5cf4ec23bb0d2a5e0ffc99e326c480d197b5c7
Sayar-tk <techdashingtv4u2b0@gmail.com>
2022-08-28 18:24:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h2 className="title">Firebase Recipe</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
